96dfa90a3ba28073b4114e0614fa7373f4c2bfad
Freenerd <nospam@freenerd.de>
2014-01-10 15:30:19 +0100

    Go fmt

--- extractor/package.go
+++ extractor/package.go
@@ -1,10 +1,10 @@
 package extractor
 
 import (
-  "os"
 	"fmt"
 	"go/build"
-  "path"
+	"os"
+	"path"
 )
 
 func PackageImportCalls(pkgName string) (map[string]map[string][]string, error) {
@@ -13,18 +13,18 @@ func PackageImportCalls(pkgName string) (map[string]map[string][]string, error) 
 		return nil, fmt.Errorf("failed to get working dir: %s", err)
 	}
 
-  imports := make(map[string]map[string][]string)
-  imports, err = processPackage(cwd, pkgName, imports)
+	imports := make(map[string]map[string][]string)
+	imports, err = processPackage(cwd, pkgName, imports)
 
-  if err != nil {
-    return nil, fmt.Errorf("failed to process package: %s", err)
-  }
+	if err != nil {
+		return nil, fmt.Errorf("failed to process package: %s", err)
+	}
 
-  return imports, nil
+	return imports, nil
 }
 
 func processPackage(root, pkgName string, imports map[string]map[string][]string) (map[string]map[string][]string, error) {
-  // read package
+	// read package
 	pkg, err := build.Import(pkgName, root, 0)
 	if err != nil {
 		return nil, fmt.Errorf("failed to import package %s: %s", pkgName, err)
@@ -35,51 +35,51 @@ func processPackage(root, pkgName string, imports map[string]map[string][]string
 		return imports, nil
 	}
 
-  // analyze each file in package, merge results
-  for _, file := range pkg.GoFiles {
-    fileImports, err := FileImportCalls(path.Join(pkg.Dir, file))
-    if err != nil {
-      return nil, fmt.Errorf("failed to in file %s: %s", file, err)
-    }
+	// analyze each file in package, merge results
+	for _, file := range pkg.GoFiles {
+		fileImports, err := FileImportCalls(path.Join(pkg.Dir, file))
+		if err != nil {
+			return nil, fmt.Errorf("failed to in file %s: %s", file, err)
+		}
 
-    imports = mergeImportMaps(imports, fileImports)
-  }
+		imports = mergeImportMaps(imports, fileImports)
+	}
 
-  // recursively extract from each imported package
-  for _, imp := range pkg.Imports {
-    // TODO: Don't do already analyzed packages
-    imports, err = processPackage(root, imp, imports)
-    if err != nil {
-      return nil, fmt.Errorf("failed to process package %s:", imp)
-    }
-  }
+	// recursively extract from each imported package
+	for _, imp := range pkg.Imports {
+		// TODO: Don't do already analyzed packages
+		imports, err = processPackage(root, imp, imports)
+		if err != nil {
+			return nil, fmt.Errorf("failed to process package %s:", imp)
+		}
+	}
 
-  return imports, nil
+	return imports, nil
 }
 
 // merge two import maps one merged output map
-func mergeImportMaps(import1, import2 map[string]map[string][]string) (map[string]map[string][]string) {
-  for imp, _ := range import1 {
-    _, present := import2[imp]
-    if present {
-      // since import is already there, we have to look at each call's array
-      for call, _ := range import1[imp] {
-        _, present := import2[imp][call]
-        if present {
-          // appending each call to call array
-          for _, e := range import1[imp][call] {
-            import2[imp][call] = append(import2[imp][call], e)
-          }
-        } else {
-          // call not there yet, use whole call array
-          import2[imp][call] = import1[imp][call]
-        }
-      }
-    } else {
-      // import not there yet, use whole import map
-      import2[imp] = import1[imp]
-    }
-  }
+func mergeImportMaps(import1, import2 map[string]map[string][]string) map[string]map[string][]string {
+	for imp, _ := range import1 {
+		_, present := import2[imp]
+		if present {
+			// since import is already there, we have to look at each call's array
+			for call, _ := range import1[imp] {
+				_, present := import2[imp][call]
+				if present {
+					// appending each call to call array
+					for _, e := range import1[imp][call] {
+						import2[imp][call] = append(import2[imp][call], e)
+					}
+				} else {
+					// call not there yet, use whole call array
+					import2[imp][call] = import1[imp][call]
+				}
+			}
+		} else {
+			// import not there yet, use whole import map
+			import2[imp] = import1[imp]
+		}
+	}
 
-  return import2
+	return import2
 }
--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"flag"
-	"log"
 	"github.com/freenerd/import-call-extractor/extractor"
+	"log"
 )
 
 var (
 	file = flag.String("f", "", "a go source file to extract calls from")
-  pkg  = flag.String("p", "", "a go package")
+	pkg  = flag.String("p", "", "a go package")
 )
 
 func main() {
@@ -18,19 +18,19 @@ func main() {
 		log.Fatal("need go source file path or go package to continue")
 	}
 
-  if *file != "" {
-    imports, err := extractor.FileImportCalls(*file)
-    if err != nil {
-      log.Fatal(err)
-      return
-    }
-    extractor.PrintYAML(imports)
-  } else if *pkg != "" {
-    imports, err := extractor.PackageImportCalls(*pkg)
-    if err != nil {
-      log.Fatal(err)
-      return
-    }
-    extractor.PrintYAML(imports)
-  }
+	if *file != "" {
+		imports, err := extractor.FileImportCalls(*file)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		extractor.PrintYAML(imports)
+	} else if *pkg != "" {
+		imports, err := extractor.PackageImportCalls(*pkg)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		extractor.PrintYAML(imports)
+	}
 }
